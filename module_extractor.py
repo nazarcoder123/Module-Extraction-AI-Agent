06e74cb504faaa073c74b9934a8cc68074597406
unknown <nazarchess14@gmail.com>
2025-04-30 21:57:23 +0530

    update

--- module_extractor.py
+++ module_extractor.py
@@ -1,5 +1,5 @@
 import argparse
-from utils.crawler import crawl
+from utils.crawler import start_crawl
 from utils.parser import extract_text_from_url
 from utils.extractor import extract_modules
 
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     all_text = ""
     for base_url in args.urls:
         print(f"Crawling {base_url}...")
-        pages = crawl(base_url)
+        pages = start_crawl(base_url)
         for page in pages:
             print(f"Parsing {page}...")
             text = extract_text_from_url(page)
